e05f14d4d83edef42487e659aa950201db84c37f
burtsevyg <burtsevyg@mail.ru>
2018-08-14 03:10:27 +0300

    fix install chef server

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://docs.chef.io/chef_overview.html
 Install chefdk on workstation:
 https://docs.chef.io/chefdk_setup.html
 
+Install chef server:
+https://docs.chef.io/install_server.html
+
--- chef-server-install.sh
+++ chef-server-install.sh
@@ -23,7 +23,7 @@ chef-manage-ctl reconfigure --accept-license
 echo "################### install opscode-push-jobs-server ###################"
 chef-server-ctl install opscode-push-jobs-server
 chef-server-ctl reconfigure
-opscode-push-jobs-server-ctl reconfigure --accept-license
+opscode-push-jobs-server-ctl reconfigure
 
 echo "################### install opscode-reporting ###################"
 chef-server-ctl install opscode-reporting
